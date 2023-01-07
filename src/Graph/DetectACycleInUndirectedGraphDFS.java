100545e15a1e5a793ab6b3f91a36911da581b7a4
codefreak <harshsaid558@gmail.com>
2023-01-07 22:51:12 +0530

    code update 7-1-23

---
+++ src/Graph/DetectACycleInUndirectedGraphDFS.java
@@ -0,0 +1,33 @@
+package Graph;
+
+import java.util.*;
+
+public class DetectACycleInUndirectedGraphDFS {
+    public boolean dfs(int node, int parent, ArrayList<ArrayList<Integer>> adj, int[] visited) {
+        visited[node] = 1;
+        for (int adjacentNode : adj.get(node)) {
+            if (visited[adjacentNode] == 0) {
+                if (dfs(adjacentNode, node, adj, visited) == true)
+                    return true;
+            }
+            else if (adjacentNode != parent)
+                return true;
+        }
+        return false;
+    }
+
+    public boolean isCycle(int v, ArrayList<ArrayList<Integer>> adj) {
+        int[] visited = new int[v];
+        for (int i = 0; i < v; i++) {
+            if (visited[i] == 0) {
+                if (dfs(i, -1, adj, visited) == true)
+                    return true;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
