fe2c8acb641cc271981f9b56ded9bbb7f35df315
codefreak <harshsaid558@gmail.com>
2023-01-21 23:17:28 +0530

    code update 21-1-23

--- src/contests/leetcode/Biweekly96/ProblemC.java
+++ src/contests/leetcode/Biweekly96/ProblemC.java
@@ -5,28 +5,7 @@ import java.util.*;
 // problem not completed
 public class ProblemC {
     public static long maxScore(int[] nums1, int[] nums2, int k) {
-        Arrays.sort(nums1);
-        Arrays.sort(nums2);
-        int n = nums1.length;
-
-        int addition = 0, temp = k;
-        for (int i = n - 1; i >= 0; i--) {
-            addition += nums1[i];
-            temp--;
-            if (temp <= 0) break;
-        }
-
-        int min = Integer.MAX_VALUE;
-        temp = k;
-        for (int i = n - 1; i >= k; i--) {
-            min = Math.min(nums2[i], min);
-            temp--;
-            if (temp <= 0) break;
-        }
-
-        System.out.println(addition);
-
-        return addition * min;
+        return 0;
     }
 
     public static void main(String[] args) {
