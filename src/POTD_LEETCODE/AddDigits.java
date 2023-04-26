2dbfb398513f2108264678efc19cb818ac3f4cf4
codefreak <harshsaid558@gmail.com>
2023-04-26 23:38:56 +0530

    code update 26-4-23

---
+++ src/POTD_LEETCODE/AddDigits.java
@@ -0,0 +1,26 @@
+package POTD_LEETCODE;
+
+public class AddDigits {
+
+    private static int getDigits(int num) {
+        int temp = 0;
+        while (num > 0) {
+            temp += num % 10;
+            num /= 10;
+        }
+        return temp;
+    }
+
+    public static int addDigits(int num) {
+        int temp = getDigits(num);
+        while (temp > 9) {
+            temp = getDigits(temp);
+        }
+        return temp;
+    }
+
+    public static void main(String[] args) {
+        int num = 0;
+        System.out.println(addDigits(num));
+    }
+}
