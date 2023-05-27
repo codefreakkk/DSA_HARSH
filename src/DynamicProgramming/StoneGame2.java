73528efacc3d9b2edfc132d0f8272221719fd759
codefreak <harshsaid558@gmail.com>
2023-05-27 15:41:09 +0530

    code update 27-3-23

--- src/DynamicProgramming/StoneGame2.java
+++ src/DynamicProgramming/StoneGame2.java
@@ -1,11 +1,14 @@
 package DynamicProgramming;
 
+import java.util.Arrays;
 
-// yet to complete
 public class StoneGame2 {
 
-    private static int solve(int[] nums, int m, int startIndex, int turn) {
+    private static int solve(int[] nums, int m, int startIndex, int turn, int[][][] dp) {
         if (startIndex >= nums.length) return 0;
+        if (dp[m][startIndex][turn] != -1) {
+            return dp[m][startIndex][turn];
+        }
 
         int ans = (turn == 0) ? 0 : Integer.MAX_VALUE;
         int takenCount = 0;
@@ -16,20 +19,27 @@ public class StoneGame2 {
             takenCount += nums[pileIndex];
 
             if (turn == 0) {
-                int count = solve(nums, Math.max(x, m), pileIndex + 1, 1);
+                int count = solve(nums, Math.max(x, m), pileIndex + 1, 1, dp);
                 int total = count + takenCount;
                 ans = Math.max(ans, total);
             }
             else {
-                int count = solve(nums, Math.max(x, m), pileIndex + 1, 0);
+                int count = solve(nums, Math.max(x, m), pileIndex + 1, 0, dp);
                 ans = Math.min(count, ans);
             }
         }
-        return ans;
+        return dp[m][startIndex][turn] = ans;
     }
 
     public static int stoneGame2(int[] nums) {
-        return solve(nums, 1, 0, 0);
+        int n = nums.length;
+
+        int[][][] dp = new int[n + 1][n + 1][2];
+        for (int[][] piles : dp) {
+            for (int[] pile : piles) Arrays.fill(pile, -1);
+        }
+
+        return solve(nums, 1, 0, 0, dp);
     }
 
     public static void main(String[] args) {
