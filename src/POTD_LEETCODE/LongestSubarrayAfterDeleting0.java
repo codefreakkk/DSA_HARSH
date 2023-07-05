9673d27637ae1f7150ad33e5823c00887ded5546
codefreak <harshsaid558@gmail.com>
2023-07-06 00:12:56 +0530

    code update 6-7-23

--- src/POTD_LEETCODE/LongestSubarrayAfterDeleting0.java
+++ src/POTD_LEETCODE/LongestSubarrayAfterDeleting0.java
@@ -2,29 +2,36 @@ package POTD_LEETCODE;
 
 public class LongestSubarrayAfterDeleting0 {
 
-    public int longestSubarray(int[] nums) {
+    public static int longestSubarray(int[] nums) {
         int n = nums.length;
 
-        int ans = 0;
+        int ans = 0, flag = 0;
         for (int i = 0; i < n; i++) {
             if (nums[i] == 0) {
                 int left = i - 1, right = i + 1;
-                int leftCount = 0, rightCount = 0;
                 while (left >= 0 && nums[left] == 1) {
                     left--;
                 }
                 while (right < n && nums[right] == 1) {
                     right++;
                 }
-                int sum = (i - left) + (right - i) - 2;
+                int sum = 0;
+                if (left == -1) {
+                    sum = i + (right - i) - 1;
+                }
+                else {
+                    sum = (i - left) + (right - i) - 2;
+                }
                 ans = Math.max(ans, sum);
+                flag = 1;
             }
         }
-        return ans;
+        return flag == 0 ? n - 1 : ans;
     }
 
 
     public static void main(String[] args) {
-
+        int[] nums = {1, 1, 1};
+        System.out.println(longestSubarray(nums));
     }
 }
