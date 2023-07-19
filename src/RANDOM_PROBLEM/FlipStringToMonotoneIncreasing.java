6e799baf25d6602971b05fb5497dbae12059517c
codefreak <harshsaid558@gmail.com>
2023-07-19 17:44:51 +0530

    code update 19-7-23

---
+++ src/RANDOM_PROBLEM/FlipStringToMonotoneIncreasing.java
@@ -0,0 +1,19 @@
+package RANDOM_PROBLEM;
+
+public class FlipStringToMonotoneIncreasing {
+
+    public int minFlips(String s) {
+        int oneCount = 0, flipCount = 0;
+
+        for (char ch : s.toCharArray()) {
+            if (ch == '1') oneCount++;
+            else {
+                flipCount++;
+                flipCount = Math.min(oneCount, flipCount);
+            }
+        }
+        return flipCount;
+    }
+
+    public static void main(String[] args) {}
+}
