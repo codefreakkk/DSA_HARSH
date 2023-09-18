11de5edbb09a8ffb94a13d740fdd802de9163822
HARSH <harshsaid558@gmail.com>
2023-09-18 13:33:54 +0530

    code update 18-9-23

--- src/leetcodeContest/WeeklyContest363/ProblemB.java
+++ src/leetcodeContest/WeeklyContest363/ProblemB.java
@@ -1,44 +1,21 @@
 package leetcodeContest.WeeklyContest363;
 import java.util.*;
-
-
-// not solved
 public class ProblemB {
 
     public static int countWays(List<Integer> nums) {
         int n = nums.size();
         Collections.sort(nums);
 
-        int num = nums.get(0);
-        boolean same = false;
-
-        for (int i = 1; i < n; i++) {
-            if (nums.get(i) != num) {
-                same = true;
-                break;
-            }
-        }
-
-        if (same) {
-            if (num < n) {
-                return 2;
-            }
-            else return 1;
-        }
-
-        int count = 0, max = num;
-        for (int it : nums) {
-            max = Math.max(max, it);
-        }
-        if (max < n) count++;
-
+        int ans = 0;
         for (int i = 0; i < n; i++) {
-            if (nums.get(i) == 0) {
-                count++;
+            if (i + 1 > nums.get(i) && i + 1 < nums.get(i + 1)) {
+                ans++;
             }
         }
-        count++;
-        return count;
+        if (nums.get(0) != 0) {
+            ans++;
+        }
+        return ans;
     }
 
     public static void main(String[] args) {
