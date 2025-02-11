17282ce3324fbd30b2c773ef1f302fe7e89ce417
codefreakkk <harshsaid558@gmail.com>
2025-02-11 22:18:25 +0530

    code update 11-02-25

---
+++ src/REVISION/LinkedList/CopyListWithRandomPointer.java
@@ -0,0 +1,61 @@
+package REVISION.LinkedList;
+
+public class CopyListWithRandomPointer {
+
+    class Node {
+        int val;
+        Node next;
+        Node random;
+
+        public Node(int val) {
+            this.val = val;
+            this.next = null;
+            this.random = null;
+        }
+    }
+
+    public Node copyRandomList(Node head) {
+        Node dummy = new Node(-1);
+        Node original = head;
+        Node clone = dummy;
+
+        while (original != null) {
+            clone.next = new Node(original.val);
+            original = original.next;
+            clone = clone.next;
+        }
+
+        original = head;
+        clone = dummy.next;
+        while (original != null && clone != null) {
+            Node temp = original.next;
+            original.next = clone;
+            original = temp;
+
+            temp = clone.next;
+            clone.next = original;
+            clone = temp;
+        }
+
+        original = head;
+        while (original != null) {
+            if (original.next != null && original.random != null) {
+                original.next.random = original.random.next;
+            }
+            original = original.next.next;
+        }
+
+        original = head;
+        clone = dummy.next;
+        while (original != null && clone != null) {
+            original.next = clone.next;
+            original = original.next;
+
+            if (original != null) {
+                clone.next = original.next;
+            }
+            clone = clone.next;
+        }
+        return dummy.next;
+    }
+}
